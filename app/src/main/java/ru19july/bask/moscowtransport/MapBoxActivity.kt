1671218c463d8eba141bbd2167dded8614369c89
Sergey Baskov <sergey.baskov@customertimes.com>
2018-09-24 19:49:45 +0300

    add MapBox

---
+++ app/src/main/java/ru19july/bask/moscowtransport/MapBoxActivity.kt
@@ -0,0 +1,60 @@
+package ru19july.bask.moscowtransport
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.constants.Style
+import com.mapbox.mapboxsdk.maps.MapView
+
+class MapBoxActivity : AppCompatActivity() {
+
+    private lateinit var mapView: MapView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Mapbox Access token
+        Mapbox.getInstance(applicationContext, getString(R.string.app_access_token))
+        setContentView(R.layout.activity_map_box)
+
+        mapView = findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync({
+            it.setStyle(Style.SATELLITE)
+            // Customize map with markers, polylines, etc.
+        })
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+
+    }
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+
+    }
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+}
