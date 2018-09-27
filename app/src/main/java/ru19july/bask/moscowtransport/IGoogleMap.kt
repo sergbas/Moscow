8433848cf3364e7170c8df3c13bf7dc03111257e
Sergey Baskov <sergey.baskov@customertimes.com>
2018-09-27 15:35:20 +0300

    restored GoogleMap and MapBox

--- app/src/main/java/ru19july/bask/moscowtransport/IGoogleMap.kt
+++
@@ -1,8 +0,0 @@
-package ru19july.bask.moscowtransport
-
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-
-interface IGoogleMap : OnMapReadyCallback, GoogleMap.OnMarkerClickListener{
-
-}
