38817c84bab038d316f8826d4881375e7bffb267
Sergey Baskov <sergey.baskov@customertimes.com>
2018-10-04 19:56:13 +0300

    refactoring - MapBoxView

--- app/src/main/java/ru19july/bask/moscowtransport/MapViewActivity.kt
+++ app/src/main/java/ru19july/bask/moscowtransport/MapViewActivity.kt
@@ -2,13 +2,9 @@ package ru19july.bask.moscowtransport
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.View
 import android.widget.LinearLayout
 import com.mapbox.mapboxsdk.Mapbox
-import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
-import com.mapbox.mapboxsdk.constants.Style
-import com.mapbox.mapboxsdk.geometry.LatLng
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
@@ -18,7 +14,9 @@ import com.yandex.mapkit.mapview.MapView
 class MapViewActivity : AppCompatActivity() {
 
     private var mapview1: MapView? = null
-    private var mapView: com.mapbox.mapboxsdk.maps.MapView? = null
+    //private var mapView: com.mapbox.mapboxsdk.maps.MapView? = null
+
+    private var map : IMapView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,27 +25,20 @@ class MapViewActivity : AppCompatActivity() {
         var layout = findViewById<View>(R.id.layout_mapview) as LinearLayout
 
         mapview1 = createYandexmap()
-        mapView = createMapboxMap()
+        //mapView = createMapboxMap()
+
+        map = MapboxView(applicationContext, getString(R.string.app_access_token), this)
+
+//        var mapview : View = map as com.mapbox.mapboxsdk.maps.MapView
 
         //layout.addView(mapview1)
-        layout.addView(mapView)
+        layout.addView(map?.getView())
 
         //moveYandexMap(mapview1)
-        moveMapbox(mapView!!)
+        //moveMapbox(map as com.mapbox.mapboxsdk.maps.MapView)
+        map?.moveTo(55.7507, 37.6177)
     }
 
-    private fun moveMapbox(mapView: com.mapbox.mapboxsdk.maps.MapView) {
-        mapView?.getMapAsync({
-
-            Log.d(javaClass.simpleName, "----- MAP -----")
-            it.setStyle(Style.SATELLITE)
-
-            val destinationCoord = LatLng(55.7507, 37.6177)
-            it.animateCamera(CameraUpdateFactory.newLatLngZoom(
-                    destinationCoord!!, 13.0));
-
-        })
-    }
 
     private fun createMapboxMap(): com.mapbox.mapboxsdk.maps.MapView {
         Mapbox.getInstance(applicationContext, getString(R.string.app_access_token))
@@ -55,9 +46,7 @@ class MapViewActivity : AppCompatActivity() {
         var mapview = com.mapbox.mapboxsdk.maps.MapView(this)
 
         mapview.setStyleUrl("mapbox://styles/mapbox/satellite-v9")
-        mapview.setLayoutParams(LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT));
+
 
         return mapview
     }
@@ -74,48 +63,46 @@ class MapViewActivity : AppCompatActivity() {
         MapKitFactory.initialize(this)
 
         var mapview = MapView(this)
-        mapview.setLayoutParams(LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT));
+
         return  mapview
     }
 
     override fun onStop() {
         super.onStop()
         mapview1?.onStop()
-        mapView?.onStop()
+        map?.onStop()
         MapKitFactory.getInstance().onStop()
     }
 
     override fun onStart() {
         super.onStart()
         mapview1?.onStart()
-        mapView?.onStart()
+        map?.onStart()
         MapKitFactory.getInstance().onStart()
     }
 
     override fun onResume() {
         super.onResume()
-        mapView?.onResume()
+        map?.onResume()
 
     }
     override fun onPause() {
         super.onPause()
-        mapView?.onPause()
+        map?.onPause()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        mapView?.onSaveInstanceState(outState)
+        map?.onSaveInstanceState(outState)
 
     }
     override fun onLowMemory() {
         super.onLowMemory()
-        mapView?.onLowMemory()
+        map?.onLowMemory()
 
     }
     override fun onDestroy() {
         super.onDestroy()
-        mapView?.onDestroy()
+        map?.onDestroy()
     }
 }
