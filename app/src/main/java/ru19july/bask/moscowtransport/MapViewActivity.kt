7ccf045bbbf8f70e116ba3656c4afb80a6164c60
Sergey Baskov <sergey.baskov@customertimes.com>
2018-10-04 19:13:38 +0300

    create mapbox mapview dynamically

--- app/src/main/java/ru19july/bask/moscowtransport/MapViewActivity.kt
+++ app/src/main/java/ru19july/bask/moscowtransport/MapViewActivity.kt
@@ -2,8 +2,13 @@ package ru19july.bask.moscowtransport
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
 import android.widget.LinearLayout
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.constants.Style
+import com.mapbox.mapboxsdk.geometry.LatLng
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
@@ -12,7 +17,8 @@ import com.yandex.mapkit.mapview.MapView
 
 class MapViewActivity : AppCompatActivity() {
 
-    private var mapview: MapView? = null
+    private var mapview1: MapView? = null
+    private var mapView: com.mapbox.mapboxsdk.maps.MapView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,11 +26,40 @@ class MapViewActivity : AppCompatActivity() {
 
         var layout = findViewById<View>(R.id.layout_mapview) as LinearLayout
 
-        mapview = createYandexmap()
+        mapview1 = createYandexmap()
+        mapView = createMapboxMap()
 
-        layout.addView(mapview)
+        //layout.addView(mapview1)
+        layout.addView(mapView)
 
-        moveYandexMap(mapview)
+        //moveYandexMap(mapview1)
+        moveMapbox(mapView!!)
+    }
+
+    private fun moveMapbox(mapView: com.mapbox.mapboxsdk.maps.MapView) {
+        mapView?.getMapAsync({
+
+            Log.d(javaClass.simpleName, "----- MAP -----")
+            it.setStyle(Style.SATELLITE)
+
+            val destinationCoord = LatLng(55.7507, 37.6177)
+            it.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                    destinationCoord!!, 13.0));
+
+        })
+    }
+
+    private fun createMapboxMap(): com.mapbox.mapboxsdk.maps.MapView {
+        Mapbox.getInstance(applicationContext, getString(R.string.app_access_token))
+
+        var mapview = com.mapbox.mapboxsdk.maps.MapView(this)
+
+        mapview.setStyleUrl("mapbox://styles/mapbox/satellite-v9")
+        mapview.setLayoutParams(LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT));
+
+        return mapview
     }
 
     private fun moveYandexMap(mapview: MapView?) {
@@ -38,8 +73,8 @@ class MapViewActivity : AppCompatActivity() {
         MapKitFactory.setApiKey("56f599ed-8d55-449e-b970-bb72f3acef84")
         MapKitFactory.initialize(this)
 
-        mapview = MapView(this)
-        mapview?.setLayoutParams(LinearLayout.LayoutParams(
+        var mapview = MapView(this)
+        mapview.setLayoutParams(LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.MATCH_PARENT));
         return  mapview
@@ -47,13 +82,40 @@ class MapViewActivity : AppCompatActivity() {
 
     override fun onStop() {
         super.onStop()
-        mapview?.onStop()
+        mapview1?.onStop()
+        mapView?.onStop()
         MapKitFactory.getInstance().onStop()
     }
 
     override fun onStart() {
         super.onStart()
-        mapview?.onStart()
+        mapview1?.onStart()
+        mapView?.onStart()
         MapKitFactory.getInstance().onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+
+    }
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+
+    }
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
     }
 }
