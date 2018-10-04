345221e24a03b78ed7158067433d8e9ad5e8f4a6
Sergey Baskov <sergey.baskov@customertimes.com>
2018-10-04 18:55:23 +0300

    create yandex mapview dynamically

--- app/src/main/java/ru19july/bask/moscowtransport/MapViewActivity.kt
+++ app/src/main/java/ru19july/bask/moscowtransport/MapViewActivity.kt
@@ -3,29 +3,46 @@ package ru19july.bask.moscowtransport
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.LinearLayout
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
 import com.yandex.mapkit.mapview.MapView
 
-class YandexMapActivity : AppCompatActivity() {
+class MapViewActivity : AppCompatActivity() {
 
     private var mapview: MapView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map_view)
 
-        MapKitFactory.setApiKey("56f599ed-8d55-449e-b970-bb72f3acef84")
-        MapKitFactory.initialize(this)
+        var layout = findViewById<View>(R.id.layout_mapview) as LinearLayout
 
-        setContentView(R.layout.activity_yandex_map)
+        mapview = createYandexmap()
 
-        mapview = (findViewById<View>(R.id.map) as MapView)
+        layout.addView(mapview)
+
+        moveYandexMap(mapview)
+    }
+
+    private fun moveYandexMap(mapview: MapView?) {
         mapview?.getMap()?.move(
                 CameraPosition(Point(55.751574, 37.573856), 12.0f, 0.0f, 0.0f),
                 Animation(Animation.Type.SMOOTH, 3F),
                 null)
+    }
+
+    private fun createYandexmap(): MapView? {
+        MapKitFactory.setApiKey("56f599ed-8d55-449e-b970-bb72f3acef84")
+        MapKitFactory.initialize(this)
+
+        mapview = MapView(this)
+        mapview?.setLayoutParams(LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT));
+        return  mapview
     }
 
     override fun onStop() {
