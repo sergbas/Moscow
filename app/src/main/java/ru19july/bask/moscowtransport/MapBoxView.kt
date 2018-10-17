78b70fc6187798fa6bfbeaec58e71459ec4a8d36
IDDQD <sergey.iddqd@yandex.ru>
2018-10-17 16:07:59 +0300

    refactoring

--- app/src/main/java/ru19july/bask/moscowtransport/MapBoxView.kt
+++ app/src/main/java/ru19july/bask/moscowtransport/MapBoxView.kt
@@ -1,2 +1,71 @@
 package ru19july.bask.moscowtransport
 
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.LinearLayout
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.constants.Style
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.maps.MapView
+
+class MapboxView(applicationContext: Context, app_access_token: String, context: Context?) : View(context), IMapView {
+    var mapview : MapView? = null
+
+    override fun getView(): MapView? {
+        return mapview
+    }
+
+    init {
+        Mapbox.getInstance(applicationContext, app_access_token)
+        mapview = MapView(applicationContext)
+        mapview?.setStyleUrl("mapbox://styles/mapbox/satellite-v9")
+
+        mapview?.setLayoutParams(LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT));
+    }
+
+    override fun onStart() {
+        mapview?.onStart()
+    }
+
+    override fun onResume() {
+        mapview?.onResume()
+    }
+
+    override fun onPause() {
+        mapview?.onPause()
+    }
+
+    override fun onStop() {
+        mapview?.onStop()
+    }
+
+    override fun onDestroy() {
+        mapview?.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        mapview?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        mapview?.onSaveInstanceState(outState)
+    }
+
+    override fun moveTo(latitude: Double, longitude: Double) {
+        mapview?.getMapAsync({
+
+            Log.d(javaClass.simpleName, "----- MAP -----")
+            it.setStyle(Style.SATELLITE)
+
+            val destinationCoord = LatLng(latitude, longitude)
+            it.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                    destinationCoord!!, 13.0));
+
+        })
+    }
+}
--- app/src/main/java/ru19july/bask/moscowtransport/MapViewActivity.kt
+++ app/src/main/java/ru19july/bask/moscowtransport/MapViewActivity.kt
@@ -1,12 +1,64 @@
 package ru19july.bask.moscowtransport
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.LinearLayout
 
 class MapViewActivity : AppCompatActivity() {
+
+    private var map : IMapView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map_view)
+
+        var layout = findViewById<View>(R.id.layout_mapview) as LinearLayout
+
+        var time = System.currentTimeMillis().toInt();
+        if(time % 1 == 0)
+            map = MapboxView(applicationContext, getString(R.string.app_access_token), this)
+        //else
+            //map = YandexmapView(this, "")
+
+        layout.addView(map?.getView())
+
+        map?.moveTo(55.7507, 37.6177)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        map?.onStop()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        map?.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        map?.onResume()
+
+    }
+    override fun onPause() {
+        super.onPause()
+        map?.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        map?.onSaveInstanceState(outState)
+
+    }
+    override fun onLowMemory() {
+        super.onLowMemory()
+        map?.onLowMemory()
+
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        map?.onDestroy()
     }
 }
+
